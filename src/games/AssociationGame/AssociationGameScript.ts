8a285d2f9921de1735cd5bc1747c6f2b20e8fd73
Froncu <jakubfratczak204@gmail.com>
2024-08-28 11:20:45 +0200

    [FEATURE] prepare `WhiteBoard` for addition of tools

--- src/games/AssociationGame/AssociationGameScript.ts
+++ src/games/AssociationGame/AssociationGameScript.ts
@@ -1,9 +1,16 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import WhiteBoard from '@/components/WhiteBoard/WhiteBoard.vue';
 
 export default defineComponent({
   name: 'AssociationGame',
   components: {
     WhiteBoard
+  },
+  setup() {
+    const whiteBoard = ref<InstanceType<typeof WhiteBoard>>();
+
+    return {
+      whiteBoard
+    };
   }
 });
